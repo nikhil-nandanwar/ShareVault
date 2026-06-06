bf6e419843332373fd473c206bf775038b96ad45
Nikhil Nandanwar <nikhilnandanwar429@gmail.com>
2026-06-06 12:42:01 +0530

    fix

--- lib/variables.ts
+++ lib/variables.ts
@@ -9,4 +9,6 @@ export const variables = {
   MONGO_USERNAME: process.env.MONGO_USERNAME,
   MONGO_PASSWORD: process.env.MONGO_PASSWORD,
   MONGO_DB_NAME: process.env.MONGO_DB_NAME,
+
+  VERCEL_URL: process.env.VERCEL_URL,
 };
